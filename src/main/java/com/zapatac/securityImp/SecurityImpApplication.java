2a71dbda8a8349257c345118eba188314e715af0
zapata <zapatac2312@gmail.com>
2024-05-18 18:46:26 -0500

    Creation of the project

---
+++ src/main/java/com/zapatac/securityImp/SecurityImpApplication.java
@@ -0,0 +1,13 @@
+package com.zapatac.securityImp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityImpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityImpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zapatac/securityImp/SecurityImpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zapatac.securityImp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityImpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
